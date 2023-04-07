02d2242122e0739173a95898bd588f2b36d4b882
Sagor Rana <sagorranait@gmail.com>
2023-04-07 06:30:37 +0600

    Job page is responsived.

--- components/WithDraw.js
+++ components/WithDraw.js
@@ -76,7 +76,7 @@ const WithDraw = () => {
                         viewBox="0 0 24 24" 
                         xmlns="http://www.w3.org/2000/svg"
                         >
-                           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                           <path stroke-linecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                         </svg>
                      <h3 className="mb-5 text-base font-normal">Are you sure you want to Withdraw?</h3>
                      <button 
--- pages/jobs/index.js
+++ pages/jobs/index.js
@@ -11,14 +11,18 @@ const jobs = () => {
       </Head>
       <main>
       <TheDivArea>
-         <div className="w-9/12 p-5 border border-silver rounded-lg">
-            <h3 className="text-2xl font-semibold pb-8">Submitted Jobs (6)</h3>
-            <div className="flex items-center justify-between border-b border-silver pb-4 mb-4">
-               <p className="text-axolotl">Initiated Mar 20, 2023</p>
-               <Link href='/works/01' className="text-primary hover:underline font-medium">Looking for an Experienced Shopify developer</Link>
+         <div className="w-4/5 lg:w-9/12 p-5 border border-silver rounded-lg">
+            <h3 className="text-base font-semibold pb-5 text-center sm:text-center sm:text-xl md:text-center lg:text-left lg:pb-8 lg:text-2xl">
+               Submitted Jobs (6)
+            </h3>
+            <div className="flex items-center justify-center lg:justify-between border-b border-silver pb-4 mb-4 flex-wrap md:flex-col lg:flex-row lg:gap-4">
+               <p className="text-axolotl">Mar 20, 2023</p>
+               <Link href='/works/01' className="text-primary hover:underline font-medium text-center py-2 lg:text-left lg:py-0">
+                  Looking for an Experienced Shopify developer
+               </Link>
                <p className="text-sm text-axolotl">Budget: $80</p>
-               <div>
-                  <Link href='/jobs/proposals/01' className="bg-primary text-white font-medium px-6 py-2 rounded-full">Proposals (5)</Link>
+               <div className="pt-1 pl-1 md:pt-3 lg:pt-0">
+                  <Link href='/jobs/proposals/01' className="bg-primary text-white font-medium px-6 py-2 rounded-full block">Proposals (5)</Link>
                </div>
             </div>
          </div>
