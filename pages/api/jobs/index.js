e3c989a6125976d7d8563a30e82ca96895d3d0d8
sagorranait <sagorranait@gmail.com>
2023-08-07 09:49:53 +0600

    Make the Jobs data sorted as DESC order.

--- pages/api/jobs/index.js
+++ pages/api/jobs/index.js
@@ -3,8 +3,9 @@ import DB, { jobs } from '@/database/connection';
 export default async function proposalsHandler(req, res) {
    try {
      await DB.connect().catch((error) => res.json({ error: 'DB Connection Failed...!' }));
+     const sort = { date: -1 };
 
-     const proposal = await jobs.find({}).toArray();
+     const proposal = await jobs.find({}).sort(sort).toArray();
      res.status(200).json(proposal);
    } catch (error) {
      res.status(500).json({ error: 'Connection Failed...!' });
